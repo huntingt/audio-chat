e9bf0df4ce641786ab8d691f1fd99851cb75a078
Parker Huntington <huntingt@mit.edu>
2021-11-20 10:36:18 -0500

    Updated fec

--- fec.py
+++ fec.py
@@ -6,43 +6,57 @@ def window(lst, interval, pad=None):
     for i in range(0, len(lst), interval):
         yield lst[i:i+interval]
 
-@np.vectorize
-def mul_m2(a, b):
-    a &= b
-    a ^= a >> 1
-    a ^= a >> 2
-    a ^= a >> 4
-    a ^= a >> 8
-    a ^= a >> 16
-    return a & 1
-
 class ConvolutionalCode:
     def __init__(self, generators, ideals):
         self.g = generators
         
         self.r = len(self.g)
-        self.K = int(max(np.log2(self.g[self.g!=0])) + 1)
-        if self.K > 31:
-            raise RuntimeError(f"K={self.K} exceds mul_m2 capability")
+        self.K = int(max(np.log2(self.g)) + 1)
 
         self.num_states = 2**(self.K - 1)
         self.mask = self.num_states - 1
 
-        self.collapse = 2**np.arange(self.r)
+        collapse = 2**np.arange(self.r)
         self.ideals = ideals
         if len(ideals) != 2**self.r:
             raise ValueError(f"the number of parity bits doesn't correspond"
                              + " with the constellation size")
 
+        @np.vectorize
+        def mul_m2(a, b):
+            return bin(a&b).count("1") & 1
+
+        @np.vectorize
+        def calculate_parity(x):
+            bits = mul_m2(self.g, x)
+            return np.dot(bits, collapse)
+
+        self.parity = calculate_parity(np.arange(2*self.num_states))
+
+        #
+        # 'x' is choosen to be a permutation of the internal states such that
+        # the resulting rows of next(x) are identity permutations. We can then
+        # calculate the previous state that correspond to each position to
+        # correctly swizzle the path metrics.
+        #
+        # x              previous      next
+        # | 0 1 2 3 |    | 0 0 2 2 |   | 1 2 3 4 |
+        # | 4 5 6 7 | -> | 1 1 3 3 | , | 1 2 3 4 |
+        #             |
+        #             |> parity matrix of x
+        #
+        x = np.reshape(np.arange(2*self.num_states), (2,self.num_states))
+
+        self.swiz_state = x >> 1
+        self.swiz_parity = self.parity[x]
+
     def encode(self, bits):
         state = 0
         samples = []
         for bit in bits:
-            x = (state << 1) | bit
-            state = x & self.mask
-
-            parity = mul_m2(self.g, x)
-            samples.append(self.ideals[np.dot(parity, self.collapse)])
+            state = (state << 1) | bit
+            samples.append(self.ideals[self.parity[state]])
+            state &= self.mask
         return samples
 
     def decode(self, samples):
@@ -52,25 +66,17 @@ class ConvolutionalCode:
         paths = []
 
         for sample in samples:
-            next_pm = np.array([np.Inf] * self.num_states)
-            path = [None] * self.num_states
-
             delta = sample - self.ideals
             delta_pm = delta.real**2 + delta.imag**2
+            candidate = pm[self.swiz_state] + delta_pm[self.swiz_parity]
             
-            for state, metric in enumerate(pm):
-                for state_0 in range(2):
-                    x = (state << 1) | state_0
-                    next_state = x & self.mask
-
-                    parity = mul_m2(self.g, x)
-                    candidate = metric + delta_pm[np.dot(parity, self.collapse)]
-
-                    if candidate < next_pm[next_state]:
-                        next_pm[next_state] = candidate
-                        path[next_state] = state
-
-            pm = next_pm
+            pm = candidate[0]
+            path = self.swiz_state[0]
+            for i in range(1, 2):
+                ls = candidate[i] < pm
+                pm = np.where(ls, candidate[i], pm)
+                path = np.where(ls, self.swiz_state[i], path)
+            
             paths.append(path)
 
         best_pm = min(pm)
@@ -118,7 +124,7 @@ if __name__ == "__main__":
     ideal = np.array(code.encode(bits))
     tx = modem.modulate(ideal)
 
-    rx = tx + 0.07 * np.random.normal(size=len(tx))
+    rx = tx + 0.01 * np.random.normal(size=len(tx))
 
     demodulated = modem.demodulate(rx)
     if len(demodulated) == 1:
@@ -129,5 +135,13 @@ if __name__ == "__main__":
         dbits = code.decode(actual)[1]
         print(f"MER = {m.MER(ideal, actual)} dB")
         print(f"BER = {sum(dbits != bits)/len(bits)}")
+
+        num = 100
+        import time
+        start = time.time()
+        for i in range(num):
+            code.decode(actual)
+        end = time.time()
+        print(f"{num} loops in {(end-start)/num} sec/loop")
     else:
         print(f"failed with {len(demodulated)} results")
